1dfd2e53c40250d070b921f30d239cd5d3919f9e
circlezero <jwy0565@gmail.com>
2019-03-19 17:52:12 +0900

    2019.03.19

--- boj/1167.py
+++ boj/1167.py
@@ -2,35 +2,11 @@ import sys
 from collections import deque
 
 n = int(sys.stdin.readline())
-d = [[] for i in range(n+1)]
-res = 0
+a = [[-1 for i in range(n+1)] for j in range(n+1)]
+dist = [0 for i in range(n+1)]
+res = 1
 
 for i in range(n):
     s = list(map(int, sys.stdin.readline().split()))
-    for j in range(1, len(s)-1, 2):
-        d[s[0]].append((s[j], s[j+1]))
-
-
-def BFS(num):
-    q.appendleft(num)
-    check[num] = 1
-
-    for i in d[num]:
-        node, dist = i
-        q.appendleft((node, dist))
-        check[node] = 1
-
-    while len(q) != 0:
-        fn, fd = q.pop()
-        for i in d[fn]:
-            node, dist = i
-            newDist = dist + fd
-            if check[node] == 0:
-                q.appendleft((node, newDist))
-
-BFS(1)
-start = 1
-
-BFS(start)
-
-print(res)
+    for j in range(1, len(s) - 1, 2):
+        dist[s[0]][s[j]] = s[j+1]
