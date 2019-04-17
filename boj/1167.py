a65b39b86f531f820d04b1474a97f40b453b3573
circlezero <jwy0565@gmail.com>
2019-04-17 17:16:51 +0900

    2019.04.17

--- boj/1167.py
+++ boj/1167.py
@@ -2,38 +2,40 @@ import sys
 from collections import deque
 
 n = int(sys.stdin.readline())
-a = [[-1 for i in range(n+1)] for j in range(n+1)]
+a = [[] for j in range(n+1)]
 dist = [-1 for i in range(n+1)]
 
 for i in range(n):
     s = list(map(int, sys.stdin.readline().split()))
     for j in range(1, len(s)-1, 2):
-        a[s[0]][s[j]] = s[j+1]
+        a[s[0]].append((s[j], s[j+1]))
 
 def BFS(num):
+    dist = [-1 for i in range(n+1)]
     dist[num] = 0
+
     q = deque()
     q.appendleft(num)
 
-    resNode = 1
+    resNode = num
     resDist = 0
     
     while len(q) != 0:
         fn = q.pop()
 
-        for i in range(1, n+1):
-            fd = a[fn][i]
-            if fd != -1 and dist[i] == -1:
-                q.appendleft(i)
-                dist[i] = dist[fn] + fd 
-                if dist[i] > resDist:
-                    resDist = dist[i]
-                    resNode = i
+        for i in a[fn]:
+            nt, nd = i
+
+            if nd != -1 and dist[nt] == -1:
+                q.appendleft(nt)
+                dist[nt] = dist[fn] + nd
+
+                if dist[nt] > resDist:
+                    resDist = dist[nt]
+                    resNode = nt
 
     return (resNode, resDist)
 
-dist = [-1 for i in range(n+1)]
 node, dist = BFS(1)
-dist = [-1 for i in range(n+1)]
 resNode, resDist = BFS(node)
 print(resDist)
