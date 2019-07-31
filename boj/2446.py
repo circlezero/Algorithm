c72d1918d4dc0680c12baed13d73cd0b0e569fd8
circlezero <jwy0565@gmail.com>
2019-07-31 23:57:10 +0900

    [BOJ] 2446 😃

---
+++ boj/2446.py
@@ -0,0 +1,16 @@
+import sys
+n = int(sys.stdin.readline())
+
+for i in range(n):
+    for j in range(i):
+        print(' ', end='')
+    for j in range(2*(n-i)-1):
+        print('*', end='')
+    print()
+
+for i in range(1, n):
+    for j in range(n-i-1):
+        print(' ', end='')
+    for j in range(2*i+1):
+        print('*', end='')
+    print()
